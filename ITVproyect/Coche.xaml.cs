c94d72f89eb49965b4021a1fefbe8f308fa35380
joan <joan@DESKTOP-8US5D37>
2022-02-06 23:18:01 +0100

    Agregar archivos de proyecto.

---
+++ ITVproyect/Coche.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ITVproyect
+{
+    /// <summary>
+    /// Lógica de interacción para Coche.xaml
+    /// </summary>
+    public partial class Coche : Window
+    {
+        public Coche()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
